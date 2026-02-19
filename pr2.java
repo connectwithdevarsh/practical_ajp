906cfa3e28fde6f6f87e7f33c894776ad35ecec8
connectwithdevarsh <connectwithdevarsh@gmail.com>
2026-02-19 14:21:26 +0530

    Add files via upload

---
+++ pr2.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+class pr2
+{
+	public static void main(String[]args)
+	{
+		JFrame f = new JFrame("Login page");
+		JLabel l = new JLabel("Enter Username");
+		JTextField t = new JTextField("");
+
+		JLabel l1 = new JLabel("Enter Password");
+		JTextField t1 = new JTextField("");
+		
+		JButton b = new JButton("Login");
+		
+		
+		l.setBounds(50,20,100,30);
+		f.add(l);
+		
+
+		t.setBounds(50,50,100,30);
+		f.add(t);
+		
+		
+		l1.setBounds(50,80,100,30);
+		f.add(l1);
+
+		t1.setBounds(50,110,100,30);
+		f.add(t1);
+
+		
+
+	
+		b.setBounds(50,150,85,30);
+		f.add(b);
+
+
+
+
+
+		f.setSize(300,300);
+		f.setLayout(null);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+
+		
+		
+	}
+}
+		
+		
+		
---
+++ pr4.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.event.*;
+class pr4
+{
+	public static void main(String[]args)
+	{
+		JFrame f = new JFrame("Login page");
+		JLabel l = new JLabel("Enter Username");
+		JTextField t = new JTextField("");
+		
+		
+		JLabel l1 = new JLabel("Enter Password");
+		JTextField t1 = new JTextField("");
+		
+		JButton b = new JButton("Login");
+		
+		
+		l.setBounds(50,20,100,30);
+		f.add(l);
+
+
+		t.setBounds(50,50,100,30);
+		f.add(t);
+		
+		
+		l1.setBounds(50,80,100,30);
+		f.add(l1);
+
+		t1.setBounds(50,110,100,30);
+		f.add(t1);
+
+		
+		b.setBounds(50,150,85,30);
+		f.add(b);
+		
+		JLabel l2 = new JLabel("");
+		l2.setBounds(50,185,100,30);
+		f.add(l2);
+
+		
+		b.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{	
+				
+				l2.setText("Welcome to "+ t.getText());
+			}
+		});
+
+
+
+		f.setSize(300,300);
+		f.setLayout(null);	
+		f.setVisible(true);
+		
+
+		
+		
+	}
+}
+		
+		
+		
